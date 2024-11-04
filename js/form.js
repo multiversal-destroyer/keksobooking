60ef2382a16bdfba23751471fd771fdad3b12071
Kirill Bondarenko <nmeshkova2007@gmail.com>
2024-11-03 23:44:29 -0600

    feat: add title validation

--- js/form.js
+++ js/form.js
@@ -1,31 +1,59 @@
-const select = document.getElementById('type');
-const checkin = document.getElementById('timein');
-const checkout = document.getElementById('timeout');
-const inputPrice = document.getElementById('price');
+const typeSelect = document.querySelector('#type');
+const checkin = document.querySelector('#timein');
+const checkout = document.querySelector('#timeout');
+const priceInput = document.querySelector('#price');
+const roomNumberSelect = document.querySelector('#room_number');
+const capacitySelect = document.querySelector('#capacity');
 
-function findOption() {
-  if (select.value === 'bungalow') {
-    inputPrice.min = "0";
-    inputPrice.placeholder = "0";
-  } else if (select.value === 'flat') {
-    inputPrice.min = "1000";
-    inputPrice.placeholder = "1000";
-  } else if (select.value === 'hotel') {
-    inputPrice.min = "3000";
-    inputPrice.placeholder = "3000";
-  } else if (select.value === 'house') {
-    inputPrice.min = "5000";
-    inputPrice.placeholder = "5000";
-  } else if (select.value === 'palace') {
-    inputPrice.min = "10000";
-    inputPrice.placeholder = "10000";
+priceInput.addEventListener('input', () => {
+  let value = parseFloat(priceInput.value);
+  let minValue = priceInput.min;
+
+  if (value > 1000000) {
+    priceInput.value = 1000000;
+  } else if (value < minValue) {
+    priceInput.value = minValue;
   }
-  if (inputPrice.value && Number(inputPrice.value) < inputPrice.min) {
-    inputPrice.value = inputPrice.min;
+});
+
+typeSelect.addEventListener('change', () => {
+  if (typeSelect.value === 'bungalow') {
+    priceInput.min = "0";
+    priceInput.placeholder = "0";
+  } else if (typeSelect.value === 'flat') {
+    priceInput.min = "1000";
+    priceInput.placeholder = "1000";
+  } else if (typeSelect.value === 'hotel') {
+    priceInput.min = "3000";
+    priceInput.placeholder = "3000";
+  } else if (typeSelect.value === 'house') {
+    priceInput.min = "5000";
+    priceInput.placeholder = "5000";
+  } else if (typeSelect.value === 'palace') {
+    priceInput.min = "10000";
+    priceInput.placeholder = "10000";
   }
-}
+  if (priceInput.value && Number(priceInput.value) < priceInput.min) {
+    priceInput.value = priceInput.min;
+  }
+});
+
 checkin.addEventListener('change', () => checkout.value = checkin.value);
 checkout.addEventListener('change', () => checkin.value = checkout.value);
 
-select.onchange = findOption;
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const titleInput = document.querySelector('#title');
 
+titleInput.addEventListener('input', () => {
+  const valueLength = titleInput.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Введите ещё ${MIN_TITLE_LENGTH - valueLength} симв.`)
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Удалите ${valueLength - MAX_TITLE_LENGTH} симв.`)
+  } else {
+    titleInput.setCustomValidity('');
+  }
+  titleInput.reportValidity();
+});
